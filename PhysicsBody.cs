3952a5f7a6f0c0a17a3b0a5894db6edc7c2f62a1
NewJumper <muhtasim.nabi@gmail.com>
2024-08-30 15:22:10 -0500

    Initial commit

---
+++ PhysicsBody.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace Physics;
+
+public class PhysicsBody {
+    private Vector2 position;
+    private float radius;
+    private float mass;
+    private Vector2 velocity;
+    private Color color;
+
+    public PhysicsBody(Vector2 position) : this(position, 20) { }
+    
+    public PhysicsBody(Vector2 position, float radius) {
+        this.position = position;
+        this.radius = radius;
+        this.mass = 0;
+        this.velocity = Vector2.Zero;
+        this.color = Color.Black;
+    }
+
+    public void SetProperties(float mass, Vector2 velocity, Color color) {
+        this.mass = mass;
+        this.velocity = velocity;
+        this.color = color;
+    }
+
+    public void VisualizeForce(Vector2 force) {
+        Raylib.DrawLineV(position, position + force, color);
+        Raylib.DrawCircleV(position, radius + 5, Program.WindowBgColor);
+    }
+
+    public void AppleForce(Vector2 force) {
+        this.velocity += force;
+    }
+
+    public void Update() {
+        position += velocity * Raylib.GetFrameTime();
+        if (position.X - radius < 0 || position.X + radius > Program.WindowSize.X) velocity = new Vector2(velocity.X * -1, velocity.Y);
+        if (position.Y - radius < 0 || position.Y + radius > Program.WindowSize.Y) velocity = new Vector2(velocity.X, velocity.Y * -1);
+    }
+
+    public void Draw() {
+        Update();
+        Raylib.DrawCircleV(position, radius, color);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace Physics;
+
+public class Program {
+    public static readonly Vector2 WindowSize = new(1280, 720);
+    public static readonly Color WindowBgColor = Color.White;
+
+    public static void Main() {
+        Raylib.InitWindow((int)WindowSize.X, (int)WindowSize.Y, "test");
+        Raylib.SetTargetFPS(60);
+
+        PhysicsBody body1 = new PhysicsBody(new Vector2(100, (int)WindowSize.Y - 100));
+
+        Vector2 clickedPos = new(0, 0);
+        bool tracking = false;
+
+        while (!Raylib.WindowShouldClose()) {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(WindowBgColor);
+            
+            if (Raylib.IsMouseButtonPressed(MouseButton.Left)) {
+                clickedPos = Raylib.GetMousePosition();
+                tracking = true;
+            }
+
+            float power = 0;
+            Vector2 direction = Vector2.Zero;
+            if (tracking) {
+                Vector2 mousePos = Raylib.GetMousePosition();
+                power = Vector2.Distance(clickedPos, mousePos);
+                direction = new Vector2(clickedPos.X - mousePos.X, clickedPos.Y - mousePos.Y);
+                if(direction.Length() != 0) direction = Vector2.Normalize(direction);
+                body1.VisualizeForce(power * direction);
+            }
+
+            if (Raylib.IsMouseButtonReleased(MouseButton.Left)) {
+                body1.AppleForce(power * direction);
+                tracking = false;
+            }
+
+            body1.Draw();
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
